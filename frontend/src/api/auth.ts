4f57041821d4190ce82424e79dae2cdcb73e3fb3
23120229 <nguyenhadatkrb2k5@gmail.com>
2025-12-03 20:06:42 +0700

    Update API URL for production

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// Địa chỉ Backend (NestJS chạy ở port 3000)
-const API_URL = "https://user-registration-u2ym.onrender.com";
-
 export const registerUser = async (userData: any) => {
   // Gửi POST request tới /user/register
   const response = await axios.post(`${API_URL}/user/register`, userData);
   return response.data;
 };
+const API_URL = "https://user-registration-u2ym.onrender.com";
